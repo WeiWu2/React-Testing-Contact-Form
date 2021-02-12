0ab5c029c96c4058117255da903349bdba83ed57
Bansa Laura <weidiwu207@gmail.com>
2021-02-11 19:53:58 -0500

    mostly almost met mvp

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -11,23 +11,43 @@ test('inputs for form', async () => {
     const lastName = screen.getByLabelText(/last name/i);
     const email = screen.getByLabelText(/email/i);
     const message = screen.getByLabelText(/message/i);
+    const submit = screen.getByRole('button', {name: /submit/i})
+
+
+
     userEvent.type(firstName, 'Wei')
     userEvent.type(lastName, 'Wu')
     userEvent.type(email, 'weiwu@lambdaschool.com')
     userEvent.type(message, 'testing is hard')
-    const submit = screen.getByRole('button', {name: /submit/i})
-        expect(firstName).toHaveValue('Wei');
-        expect(lastName).toHaveValue('Wu');
-        expect(email).toHaveValue('weiwu@lambdaschool.com');
-        expect(message).toHaveValue('testing is hard');
+    expect(firstName).toHaveValue('Wei');
+    expect(lastName).toHaveValue('Wu');
+    expect(email).toHaveValue('weiwu@lambdaschool.com');
+    expect(message).toHaveValue('testing is hard');
+
+
+
        userEvent.click(submit);
-        const obj = {
-            firstName: 'Wei', 
-            lastName: 'Wu',
-            email: 'weiwu@lambdaschool.com',
-            message: 'testing is hard'
-        }
         const newInfo = await screen.findByText(/Wei/i)
         expect(newInfo).toBeInTheDocument();
+
+})
+
+
+test('form validation', async () => {
+    const {debug } = render(<ContactForm />)
+
+    const firstName = screen.getByLabelText(/first name/i);
+    const lastName = screen.getByLabelText(/last name/i);
+    const email = screen.getByLabelText(/email/i);
+    const message = screen.getByLabelText(/message/i);
+    const submit = screen.getByRole('button', {name: /submit/i})
+
+    userEvent.type(firstName, 'Weiwwwwwwww')
+    expect(firstName).toHaveValue('Weiwwwwwwww');
+    userEvent.click(submit);
+    const error = await screen.queryByText(/Weiwwwwwwww/i)
+    expect(error).toBeNull();
+  
+
 
 })
